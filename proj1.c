8a94f72ce5115bd4fb65ed771779446d475247dd
Harry Flynn <harry.flynn1109@gmail.com>
2019-04-04 05:35:02 +0000

    Rotation encryption only  (function and main).

--- proj1.c
+++ proj1.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 //rotation cipher test
+char encryption(char *message);
+
 int main() {
-    int shift = 6; //Initialising shift amount to 6 places.
-    int index = 0; //Initialising string index to zero (does not need to be initialised, only decalred).
     char message[13] = {'H', 'E', 'L', 'L', 'O', ' ', 'W', 'O', 'R', 'L', 'D', '.', '\0'};
+    message[13] = encryption(message);
+    printf("Encryption: %s", message);
+}
+
+char encryption(char *message) {
+    int shift = 6; //Initialising shift amount to 6 places.
+    int index; //Initialising string index to zero (does not need to be initialised, only decalred).
     printf("Message: %s\n", message);
     printf("Rotation key: %d.\n", shift);
     for(index = 0; index < 13; index++) {
-        if(message[index] < 65 || message[index] > 90) {
-            message[index] = message[index];
+        if(message[index] < 65 || message[index] > 90) {    //If the letter is not between A-Z, keep its value
+            message[index] = message[index];           
         }
-        else if (message[index] + shift > 90) {
+        else if (message[index] + shift > 90) {             //If the new value goes past Z, finish rotation from A
             int difference = 90 - message[index];
             message[index] = 64 + difference;
         }
-        else {
+        else {                                              //Add the shift value to rotate each other letter.
             message[index] = message[index] + shift;
         }
 
     }
-    printf("Encryption: %s", message);
+    return *message;
 }    
