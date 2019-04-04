79e6589dfe4177cbe814d1e36b14ccc5f93fa0cb
Harry Flynn <harry.flynn1109@gmail.com>
2019-04-04 05:54:35 +0000

    Added menu/options. Rotation Cipher only.

--- proj1.c
+++ proj1.c
@@ -3,16 +3,34 @@
 char encryption(char *message);
 
 int main() {
+    int task; //Integer for storing selected task
+    printf("Select a task: \n");
+    printf("1. Encryption of a message with a rotation cipher given the message text and rotation amount.\n");
+    printf("2. Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount.\n");
+    printf("3. Encryption of a message with a substitution cipher given message text and alphabet substitution.\n");
+    printf("4. Decryption of a message encrypted with a substitution cipher given cipher text and substitutions.\n");
+    printf("5. Decryption of a message encrypted with a rotation cipher given cipher text only.\n");
+    printf("6. Decryption of a message encrypted with a substitution cipher given cipher text only.\n");
+    scanf("%d", &task);
     char message[13] = {'H', 'E', 'L', 'L', 'O', ' ', 'W', 'O', 'R', 'L', 'D', '.', '\0'};
-    message[13] = encryption(message);
-    printf("Encryption: %s", message);
+    
+    switch(task)
+    {
+        case 1: message[13] = encryption(message); printf("    Encryption: %s", message);
+        case 2: break;
+        case 3: break; 
+        case 4: break; 
+        case 5: break; 
+        case 6: break;
+    }    
 }
 
 char encryption(char *message) {
     int shift = 6; //Initialising shift amount to 6 places.
     int index; //Initialising string index to zero (does not need to be initialised, only decalred).
-    printf("Message: %s\n", message);
-    printf("Rotation key: %d.\n", shift);
+    printf("Rotation Cipher:\n");
+    printf("    Message: %s\n", message);
+    printf("    Rotation key: %d.\n", shift);
     for(index = 0; index < 13; index++) {
         if(message[index] < 65 || message[index] > 90) {    //If the letter is not between A-Z, keep its value
             message[index] = message[index];           
@@ -27,4 +45,4 @@ char encryption(char *message) {
 
     }
     return *message;
-}    
+}   
