bbf1941fb47c6b9dc84b8e4ed3a03088127f8599
Harry Flynn <harry.flynn1109@gmail.com>
2019-04-04 05:05:26 +0000

    Encryption working for rotation cipher (all in main).

--- proj1.c
+++ proj1.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
-
+//rotation cipher test
 int main() {
+    int shift = 6; //Initialising shift amount to 6 places.
+    int index = 0; //Initialising string index to zero (does not need to be initialised, only decalred).
+    char message[13] = {'H', 'E', 'L', 'L', 'O', ' ', 'W', 'O', 'R', 'L', 'D', '.', '\0'};
+    printf("Message: %s\n", message);
+    printf("Rotation key: %d.\n", shift);
+    for(index = 0; index < 13; index++) {
+        if(message[index] < 65 || message[index] > 90) {
+            message[index] = message[index];
+        }
+        else if (message[index] + shift > 90) {
+            int difference = 90 - message[index];
+            message[index] = 64 + difference;
+        }
+        else {
+            message[index] = message[index] + shift;
+        }
 
-  return 0;
-}
-/////kuythrgf
+    }
+    printf("Encryption: %s", message);
+}    
