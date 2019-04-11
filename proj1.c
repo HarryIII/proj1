9b697d3bb420cc0ff845af5524052e3b8256c8e1
Harry Flynn <harry.flynn1109@gmail.com>
2019-04-11 05:18:08 +0000

    Rotation Decryption added but not quite working (reading encrypted message as an empty string).

--- proj1.c
+++ proj1.c
@@ -1,48 +1,82 @@
 #include <stdio.h>
 //rotation cipher test
-char encryption(char *message);
+char RotationEncryption(char *message, char *rotationEncryptionMessage);
+char RotationDecryption(char *rotationEncryptionMessage, char *rotationDecryptionMessage);
 
 int main() {
     int task; //Integer for storing selected task
-    printf("Select a task: \n");
+  /*printf("Select a task: \n");
     printf("1. Encryption of a message with a rotation cipher given the message text and rotation amount.\n");
     printf("2. Decryption of a message encrypted with a rotation cipher given cipher text and rotation amount.\n");
     printf("3. Encryption of a message with a substitution cipher given message text and alphabet substitution.\n");
     printf("4. Decryption of a message encrypted with a substitution cipher given cipher text and substitutions.\n");
     printf("5. Decryption of a message encrypted with a rotation cipher given cipher text only.\n");
     printf("6. Decryption of a message encrypted with a substitution cipher given cipher text only.\n");
-    scanf("%d", &task);
+    scanf("%d", &task);*/
+    task = 2; //manually setting task to bypasa input for now.
     char message[13] = {'H', 'E', 'L', 'L', 'O', ' ', 'W', 'O', 'R', 'L', 'D', '.', '\0'};
-    
+    char rotationEncryptionMessage[13] = {'N', 'K', 'R', 'R', 'U', ' ', 'C', 'U', 'X', 'R', 'J', '.', '\0'};
+    printf("%s\n", rotationEncryptionMessage);
+    char rotationDecryptionMessage[13] = {};
     switch(task)
     {
-        case 1: message[13] = encryption(message); printf("    Encryption: %s", message);
-        case 2: break;
+        case 1: rotationEncryptionMessage[13] = RotationEncryption(message, rotationEncryptionMessage); 
+            printf("    Encryption: %s\n", rotationEncryptionMessage); break;
+        case 2: rotationDecryptionMessage[13] = RotationDecryption(rotationDecryptionMessage, rotationEncryptionMessage); 
+            printf("    Decryption: %s\n", rotationDecryptionMessage); break;
         case 3: break; 
         case 4: break; 
         case 5: break; 
         case 6: break;
-    }    
+    }
 }
 
-char encryption(char *message) {
+//RotationEncryption takes a string and shifts each character in that string by a roation amount (e.g. 6), ignoring non characters that...
+//...are not letters.
+
+char RotationEncryption(char *message, char *rotationEncryptionMessage) {
     int shift = 6; //Initialising shift amount to 6 places.
-    int index; //Initialising string index to zero (does not need to be initialised, only decalred).
-    printf("Rotation Cipher:\n");
+    int index; //Declaring string index.
+    printf("Rotation Cipher Encryption:\n");
     printf("    Message: %s\n", message);
     printf("    Rotation key: %d.\n", shift);
     for(index = 0; index < 13; index++) {
         if(message[index] < 65 || message[index] > 90) {    //If the letter is not between A-Z, keep its value
-            message[index] = message[index];           
+            rotationEncryptionMessage[index] = message[index];           
         }
         else if (message[index] + shift > 90) {             //If the new value goes past Z, finish rotation from A
             int difference = 90 - message[index];
-            message[index] = 64 + difference;
+            rotationEncryptionMessage[index] = 64 + difference;
         }
         else {                                              //Add the shift value to rotate each other letter.
-            message[index] = message[index] + shift;
+            rotationEncryptionMessage[index] = message[index] + shift;
         }
 
     }
-    return *message;
+    return *rotationEncryptionMessage;
 }   
+
+//RotationDecrption takes the encrypted message from RotationEncryption and shifts each letter back to it's original meaning.
+
+
+char RotationDecryption(char *rotationEncryptionMessage, char *rotationDecryptionMessage) {
+    int shift = 6;
+    int index;
+    printf("Rotation Cipher Decryption:\n");
+    printf("    Encrypted Message: %s\n", rotationEncryptionMessage);
+    printf("    Rotation key: %d\n", shift);
+    for(index = 0; index < 13; index++) {
+        if(rotationEncryptionMessage[index] < 65 || rotationEncryptionMessage[index] > 90) {    //If the letter is not between A-Z, keep its value
+            rotationDecryptionMessage[index] = rotationEncryptionMessage[index];           
+        }
+        else if (rotationEncryptionMessage[index] - shift < 60) {             //If the new value goes past Z, finish rotation from A
+            int difference = 64 - rotationEncryptionMessage[index];
+            rotationDecryptionMessage[index] = 90 - difference;
+        }
+        else {                                              //Add the shift value to rotate each other letter.
+            rotationDecryptionMessage[index] = rotationEncryptionMessage[index] - shift;
+        }
+
+    }
+    return *rotationDecryptionMessage;
+}
